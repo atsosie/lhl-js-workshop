3728a202a3fa72f402ad3e02489d547bc3380ac9
Amber Tsosie <atxambra@gmail.com>
2019-01-15 15:01:36 -0800

    Installed socket.io for real-time messaging and added basic code to handle the message input.

--- client/app.js
+++ client/app.js
@@ -1,5 +1,21 @@
-$('form').on('submit',function () {
-  var text = $('#message').val();
-  alert(text);
+var socket = io();
+
+/* Emit textual message to the server and clear the input so
+ * another message can be typed by the same user.
+ */
+$("form").on("submit", function() {
+  var text = $("#message").val();
+  socket.emit("message", text);
+  $("#message").val("");
   return false;
+});
+
+/* Any time a message is received from the real time web socket
+ * connection with the server, create a new <li> element and
+ * append it to the message history <ol>.
+ */
+socket.on("message", function(msg) {
+  $("<li>")
+    .text(msg)
+    .appendTo("#history");
 });
